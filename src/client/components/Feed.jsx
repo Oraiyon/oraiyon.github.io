f7fb681af363802f20b2c11aed05046a3d0379b5
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-12-27 11:28:26 +0800

    Created useEffect() that gets posts of followed users

--- src/client/components/Feed.jsx
+++ src/client/components/Feed.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
 import { Link, useOutletContext } from "react-router-dom";
+import PostList from "./PostList";
+import styles from "../stylesheets/Feed.module.css";
 
 const Feed = () => {
-  const [user, setUser, previousPage, setPreviousPage] = useOutletContext();
+  const [user, setUser, post, setPost] = useOutletContext();
 
-  return (
-    <>
-      <h1>{user.username}</h1>
-    </>
-  );
+  const [followingPosts, setFollowingPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchFeed = async () => {
+      try {
+        const response = await fetch(`/api/${user.id}/get/following/posts`);
+        const data = await response.json();
+        setFollowingPosts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (user) {
+      fetchFeed();
+    }
+  }, []);
+
+  if (user && followingPosts) {
+    return (
+      <div className={styles.feed_container}>
+        <PostList followingPosts={followingPosts} />
+      </div>
+    );
+  }
 };
 
 export default Feed;
