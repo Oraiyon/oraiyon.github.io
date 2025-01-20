9f37441bac14cb7a30b939a23455413e7b18ecd4
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-01-20 13:06:30 +0800

    Restructured Links to profiles. Profile pictures are now also Links

--- src/client/components/ToProfile.jsx
+++ src/client/components/ToProfile.jsx
@@ -4,14 +4,23 @@ import DisplayProfilePicture from "./DisplayProfilePicture";
 
 const ToProfile = (props) => {
   return (
-    <div className={styles.user_card}>
-      <DisplayProfilePicture user={props.searchedUser} />
+    <>
       {!props.user || props.user.username !== props.searchedUser.username ? (
-        <Link to={`/${props.searchedUser.id}/profile`}>{props.searchedUser.username}</Link>
+        <Link to={`/${props.searchedUser.id}/profile`}>
+          <div className={styles.user_card}>
+            <DisplayProfilePicture user={props.searchedUser} />
+            <p>{props.searchedUser.username}</p>
+          </div>
+        </Link>
       ) : (
-        <Link to={`/user`}>{props.searchedUser.username}</Link>
+        <Link to={`/user`}>
+          <div className={styles.user_card}>
+            <DisplayProfilePicture user={props.searchedUser} />
+            <p>{props.searchedUser.username}</p>
+          </div>
+        </Link>
       )}
-    </div>
+    </>
   );
 };
 
