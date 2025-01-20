04468510a711042fd7d9dd5c708adaa78488b919
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-01-20 12:17:26 +0800

    Imported <DisplayProfilePicture />

--- src/client/components/ToProfile.jsx
+++ src/client/components/ToProfile.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import styles from "../stylesheets/ToProfile.module.css";
+import DisplayProfilePicture from "./DisplayProfilePicture";
 
 const ToProfile = (props) => {
   return (
     <div className={styles.user_card}>
+      <DisplayProfilePicture user={props.searchedUser} />
       {!props.user || props.user.username !== props.searchedUser.username ? (
         <Link to={`/${props.searchedUser.id}/profile`}>{props.searchedUser.username}</Link>
       ) : (
