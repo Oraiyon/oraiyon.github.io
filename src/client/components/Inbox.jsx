de7b71dc8f8519c8570373183890040c2e43dcae
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-01-22 14:41:35 +0800

    Inbox.jsx now displays follower info

--- src/client/components/Inbox.jsx
+++ src/client/components/Inbox.jsx
@@ -1,11 +1,28 @@
+import { useEffect } from "react";
 import styles from "../stylesheets/Inbox.module.css";
+import { useOutletContext } from "react-router-dom";
+import DisplayDate from "./DisplayDate";
 
 const Inbox = () => {
-  return (
-    <div className={styles.inbox_container}>
-      <h1>Inbox Page</h1>
-    </div>
-  );
+  const [user, setUser, post, setPost] = useOutletContext();
+
+  useEffect(() => {
+    console.log(user);
+  }, []);
+
+  if (user) {
+    return (
+      <div className={styles.inbox_container}>
+        <h1>Inbox Page</h1>
+        {user.FollowedBy.map((follower) => (
+          <div key={follower.id} className={styles.follower_notification}>
+            <p>{follower.receiver.username}</p>
+            <DisplayDate date={follower.followedDate} />
+          </div>
+        ))}
+      </div>
+    );
+  }
 };
 
 export default Inbox;
