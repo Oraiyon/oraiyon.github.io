09d4ba2432364a8a6037fbfc34b424df2c09cb58
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-01-22 11:38:43 +0800

    Removed useEffect(). Logging in already gets follow data.

--- src/client/components/User.jsx
+++ src/client/components/User.jsx
@@ -2,25 +2,9 @@ import { useOutletContext } from "react-router-dom";
 import PostList from "./PostList";
 import ProfileHeader from "./ProfileHeader";
 import styles from "../stylesheets/User.module.css";
-import { useEffect } from "react";
 
 const User = () => {
   const [user, setUser, post, setPost] = useOutletContext();
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await fetch(`/api/${user.id}/profile`);
-        const data = await response.json();
-        setUser(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    if (user) {
-      fetchUser();
-    }
-  }, []);
 
   if (user) {
     return (
