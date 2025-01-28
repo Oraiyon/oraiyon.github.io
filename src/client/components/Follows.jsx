8c2b236334617737f699f19729b9e70026a87325
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-01-28 12:42:46 +0800

    Switched props.userProfile

--- src/client/components/Follows.jsx
+++ src/client/components/Follows.jsx
@@ -1,20 +1,23 @@
 import { useEffect } from "react";
 import styles from "../stylesheets/Follows.module.css";
+import { Link } from "react-router-dom";
 
 const Follows = (props) => {
   useEffect(() => {
-    // console.log(props.followers);
+    console.log(props.userProfile);
   }, []);
 
   return (
     <div className={styles.follow_container}>
-      <div className={styles.followers_container}>
-        <p>Followers</p>
-        <p>{props.followers.length}</p>
-      </div>
+      <Link to={`/${props.userProfile.id}/followers`}>
+        <div className={styles.followers_container}>
+          <p>Followers</p>
+          <p>{props.userProfile.Following.length}</p>
+        </div>
+      </Link>
       <div className={styles.following_container}>
         <p>Following</p>
-        <p>{props.following.length}</p>
+        <p>{props.userProfile.Followers.length}</p>
       </div>
     </div>
   );
