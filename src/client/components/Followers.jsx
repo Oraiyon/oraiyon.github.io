483f86ae4c361493fd8a6e10eabcf58be34c8cb6
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-01-28 14:55:46 +0800

    Updated Followers.jsx to display followers

--- src/client/components/Followers.jsx
+++ src/client/components/Followers.jsx
@@ -1,3 +1,40 @@
-const Followers = () => {};
+import { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import styles from "../stylesheets/Followers.module.css";
+import ToProfile from "./ToProfile";
+
+const Followers = () => {
+  const [user, setUser, post, setPost] = useOutletContext();
+
+  const [userFollowers, setUserFollowers] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(`/api/${window.location.pathname.split("/")[1]}/followers`);
+        const data = await response.json();
+        console.log(data);
+        setUserFollowers(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  if (userFollowers) {
+    return (
+      <>
+        <div className={styles.followers_container}>
+          {userFollowers.map((follow) => (
+            <div key={follow.id}>
+              <ToProfile searchedUser={follow} />
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
+};
 
 export default Followers;
