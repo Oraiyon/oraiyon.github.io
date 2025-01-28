8e8932e530aa347c0af98c773102a2fc674794e3
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-01-28 12:40:35 +0800

    Updated controllers. Reversed params as it was confusing before

--- src/server/controllers/followControllers.js
+++ src/server/controllers/followControllers.js
@@ -6,8 +6,8 @@ const prisma = new PrismaClient();
 export const get_follow = expressAsyncHandler(async (req, res, next) => {
   const following = await prisma.follow.findFirst({
     where: {
-      senderId: req.params.receiver,
-      receiverId: req.params.sender
+      senderId: req.params.sender,
+      receiverId: req.params.receiver
     }
   });
   res.status(200).json(following);
@@ -16,10 +16,10 @@ export const get_follow = expressAsyncHandler(async (req, res, next) => {
 export const get_followers = expressAsyncHandler(async (req, res, next) => {
   const followersList = await prisma.follow.findMany({
     where: {
-      senderId: req.params.id
+      receiverId: req.params.id
     },
     include: {
-      receiver: true
+      sender: true
     }
   });
   res.status(200).json(followersList);
@@ -28,10 +28,10 @@ export const get_followers = expressAsyncHandler(async (req, res, next) => {
 export const get_following = expressAsyncHandler(async (req, res, next) => {
   const followingList = await prisma.follow.findMany({
     where: {
-      receiverId: req.params.id
+      senderId: req.params.id
     },
     include: {
-      sender: true
+      receiver: true
     }
   });
   res.status(200).json(followingList);
