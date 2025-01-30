c8018ac7d927fc4b8191c24b1654f67590a9b049
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-01-30 14:52:41 +0800

    Imported <ToProfile/> in Inbox.jsx

--- src/client/components/Inbox.jsx
+++ src/client/components/Inbox.jsx
@@ -2,20 +2,19 @@ import { useEffect } from "react";
 import styles from "../stylesheets/Inbox.module.css";
 import { useOutletContext } from "react-router-dom";
 import DisplayDate from "./DisplayDate";
+import ToProfile from "./ToProfile";
 
 const Inbox = () => {
   const [user, setUser, post, setPost] = useOutletContext();
 
-  useEffect(() => {
-    // console.log(user.Following);
-  }, []);
+  useEffect(() => {}, []);
 
   if (user) {
     return (
       <div className={styles.inbox_container}>
         {user.Following.map((follower) => (
           <div key={follower.id} className={styles.follower_notification}>
-            <p>{follower.sender.username}</p>
+            <ToProfile searchedUser={follower.sender} user={null} />
             <DisplayDate date={follower.followedDate} />
           </div>
         ))}
