3296364a8923ff2c4eabea12e0128c3c638bbae8
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-01-30 13:58:07 +0800

    Created <Following />

--- src/client/components/Following.jsx
+++ src/client/components/Following.jsx
@@ -1,11 +1,52 @@
 import styles from "../stylesheets/Following.module.css";
+import { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import ToProfile from "./ToProfile";
+import BackHeader from "./BackHeader";
 
 const Following = () => {
-  return (
-    <>
-      <h1>FOLLOWING PAGE</h1>
-    </>
-  );
+  const [user, setUser, post, setPost] = useOutletContext();
+
+  const [userFollowing, setUserFollowing] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(`/api/${window.location.pathname.split("/")[1]}/following`);
+        const data = await response.json();
+        setUserFollowing(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  if (userFollowing.length) {
+    return (
+      <>
+        <div className={styles.following_container}>
+          <BackHeader mode={"profile"} />
+          <div>
+            {userFollowing.map((follow) => (
+              <div key={follow.id} className={styles.following_card}>
+                <ToProfile searchedUser={follow} user={null} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className={styles.following_container}>
+          <BackHeader mode={"profile"} />
+          <p>No Following.</p>
+        </div>
+      </>
+    );
+  }
 };
 
 export default Following;
