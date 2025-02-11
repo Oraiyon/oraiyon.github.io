726c6fa58a29dc5e835615588aaf1097724103ed
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-02-11 11:37:33 +0800

    Added a link to "/".

--- src/client/components/ErrorPage.jsx
+++ src/client/components/ErrorPage.jsx
@@ -1,10 +1,14 @@
 import styles from "../stylesheets/ErrorPage.module.css";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
     <div className={styles.error_page_container}>
       <h1>ERROR</h1>
       <p>PAGE NOT FOUND</p>
+      <button>
+        <Link to="/">Go Home</Link>
+      </button>
     </div>
   );
 };
